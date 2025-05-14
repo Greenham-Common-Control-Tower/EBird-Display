c16539a9b05f48ff74967e4f4931acc2f9f52ab5
Darbyshire64 <darbyshire.lawri>
2025-05-14 21:52:04 +0100

    Made the Data Fetcher

--- main.py
+++ main.py
@@ -5,7 +5,16 @@ import json
 # Edit These Flags To Customize the software
 PORT=1991
 KEY="6ohcvqhs11a7"
+LOCATION=""
 
+#Begining of Function Definitions
+def fetchData():
+    headers = {'X-eBirdApiToken': KEY}
+    response = requests.get(f"https://api.ebird.org/v2/data/obs/{LOCATION}/recent", headers=headers)
+    data = response.text
+    return data
+
+## Begining of Active Code
 print("Initializing Web Server")
 app = Flask(__name__)
 
